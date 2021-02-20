f3dfb11eb48d3809f514e2dd6677b7d2489e2e64
IMBABOT1 <zlovred12@gmail.com>
2021-02-20 16:48:33 +0300

    productService

---
+++ src/main/java/ru/geekbrains/spring1/Service/ProductService.java
@@ -0,0 +1,27 @@
+package ru.geekbrains.spring1.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.geekbrains.spring1.Model.Product;
+import ru.geekbrains.spring1.Repository.ProductRepository;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAll(){
+        return productRepository.getAll();
+    }
+
+    public Product getById(int id){
+        return productRepository.getById(id);
+    }
+
+}
