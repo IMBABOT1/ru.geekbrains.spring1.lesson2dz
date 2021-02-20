583db2361c410c0684b8409d8084ec8cd4175f78
IMBABOT1 <zlovred12@gmail.com>
2021-02-20 17:18:33 +0300

    thymeleaf

--- src/main/java/ru/geekbrains/spring1/Controller/ProductController.java
+++ src/main/java/ru/geekbrains/spring1/Controller/ProductController.java
@@ -1,13 +1,29 @@
 package ru.geekbrains.spring1.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.geekbrains.spring1.Model.Product;
+import ru.geekbrains.spring1.Service.ProductService;
+
+import javax.swing.*;
+import java.util.List;
 
 @Controller
 public class ProductController {
 
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/get_products")
-    public String getAllProducts(){
+    public String getAllProducts(Model model){
+        List<Product> products = productService.getAll();
+        model.addAttribute("products", products);
         return "product";
     }
 }
